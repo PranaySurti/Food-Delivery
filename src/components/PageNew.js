965a711d1c7938c8cbaf478edabbe911d178933c
the_Sunny_Sharma_ <syn8433898723@gmail.com>
2023-11-02 21:48:02 +0530

    Added emailJS Functionalities

--- src/components/PageNew.js
+++ src/components/PageNew.js
@@ -1,16 +1,70 @@
 import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 export default function PageNew() {
   const currentDate = new Date(); // Represents the current date and time
   const formattedDateTime = currentDate.toLocaleString();
 
+  const rName = useRef();
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [mail, setMail] = useState("");
+
+  const hName = (event) => {
+    setName(event.target.value);
+  };
+  const hPhone = (event) => {
+    setPhone(event.target.value);
+  };
+  const hMail = (event) => {
+    setMail(event.target.value);
+  };
+
+  const save = (event) => {
+    event.preventDefault();
+    let data = { name: name, phone: phone, mail: mail };
+    emailjs
+      .send("service_f9mlxe5", "template_yistcoe", data, "LRJLiZUKC5pBoTYvK")
+      .then((res) => {
+        alert("we will get back to u soon");
+        setName("");
+        setMail("");
+        setPhone("");
+        rName.current.focus();
+      })
+      .catch((err) => console.log("issue " + err));
+  };
   return (
     <>
       <center>
         <h1>Transaction has been completed</h1>
         <p>Transaction Id: JBISWB723BJK</p>
         <p>Current Date and Time: {formattedDateTime}</p>
-        <br/><br/>
+        <br />
+        <br />
+
+          <input
+            type="text"
+            placeholder="Enter your Name"
+            value={name}
+            onChange={hName}
+          />
+          <input
+            type="number"
+            placeholder="Mobile Number"
+            value={phone}
+            onChange={hPhone}
+          />
+          <input
+            type="text"
+            placeholder="Email "
+            value={mail}
+            onChange={hMail}
+          />
+          <br /><br/>
+          <button onClick={save}>SAVE</button>
+        <br /><br/>
         <Link to="/">Go to Home Page</Link>
       </center>
     </>
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,15 +2,24 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyChTmLu1Dwmpjy89xSRrB4kWJk4JkJOzAI",
+//   authDomain: "food-delivery-a4043.firebaseapp.com",
+//   databaseURL: "https://food-delivery-a4043-default-rtdb.firebaseio.com",
+//   projectId: "food-delivery-a4043",
+//   storageBucket: "food-delivery-a4043.appspot.com",
+//   messagingSenderId: "181667256978",
+//   appId: "1:181667256978:web:1f7eec5b7a48bbfbec244b"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyChTmLu1Dwmpjy89xSRrB4kWJk4JkJOzAI",
-  authDomain: "food-delivery-a4043.firebaseapp.com",
-  databaseURL: "https://food-delivery-a4043-default-rtdb.firebaseio.com",
-  projectId: "food-delivery-a4043",
-  storageBucket: "food-delivery-a4043.appspot.com",
-  messagingSenderId: "181667256978",
-  appId: "1:181667256978:web:1f7eec5b7a48bbfbec244b"
-};
+  apiKey: "AIzaSyCaqoAyQ66odXZdvxBsLMf2_ePCc4Ycn7I",
+  authDomain: "restaurantapp-fe54e.firebaseapp.com",
+  databaseURL: "https://restaurantapp-fe54e-default-rtdb.firebaseio.com",
+  projectId: "restaurantapp-fe54e",
+  storageBucket: "restaurantapp-fe54e.appspot.com",
+  messagingSenderId: "341867919550",
+  appId: "1:341867919550:web:45b5b55532a3a875ea06ef"
+  };
 
   
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
